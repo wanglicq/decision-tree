b835d0307a75c504614b0298997be8ba4d106b53
wangli <wangli_cq@hotmail.com>
2018-07-05 15:41:22 +0800

    Build decision tree for watermelon classification.

--- src/demo.py
+++ src/demo.py
@@ -1,44 +1,57 @@
 import pandas as pd
 from sklearn import model_selection
 from sklearn import tree
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import metrics
 import graphviz
 
-# Step 1, read data
-# read csv data
+#
 watermelonData = pd.read_csv('../docs/waterMelon.csv')
-# read first 5 lines data
-#print(watermelonData.head())
-# get data dimension
-#print(watermelonData.shape)
+
+data = pd.DataFrame(watermelonData)
+
+dataTransfor = LabelEncoder()
+
+#选择数据特性
+watermelonCharactor = data[['色泽', '根蒂', '敲声', '纹理', '脐部', '触感', '密度', '含糖率']].values
+# 把字符串都转化为整型
+watermelonCharactor[:, 0] = dataTransfor.fit_transform(watermelonCharactor[:, 0])
+watermelonCharactor[:, 1] = dataTransfor.fit_transform(watermelonCharactor[:, 1])
+watermelonCharactor[:, 2] = dataTransfor.fit_transform(watermelonCharactor[:, 2])
+watermelonCharactor[:, 3] = dataTransfor.fit_transform(watermelonCharactor[:, 3])
+watermelonCharactor[:, 4] = dataTransfor.fit_transform(watermelonCharactor[:, 4])
+watermelonCharactor[:, 5] = dataTransfor.fit_transform(watermelonCharactor[:, 5])
+watermelonCharactor[:, 6] = dataTransfor.fit_transform(watermelonCharactor[:, 6])
+watermelonCharactor[:, 7] = dataTransfor.fit_transform(watermelonCharactor[:, 7])
+#数据特性进行独热编码
+watermelonX = OneHotEncoder(categorical_features=[0], sparse=False).fit_transform(watermelonCharactor)
 
 
-# Step 2, prepare data
-# select sample characteristic
-X = watermelonData[['色泽', '根蒂', '敲声', '纹理', '脐部', '触感', '密度', '含糖率']]
-
-# select sample output
-y = watermelonData[['好瓜']]
-
-# Step 3, split data into training set and testing set
-# cross_validation是交叉验证
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.4)
-print(X_train)
-print(y_train)
-print(X_test.shape)
-print(y_test.shape)
+#选择样本输出
+watermelonOutput = data[['好瓜']].values
+watermelonOutput[:, 0] = dataTransfor.fit_transform(watermelonOutput[:, 0])
+watermelonY = OneHotEncoder(categorical_features=[0], sparse=False).fit_transform(watermelonOutput)
 
 
-# Step 4, train the data, get tree
+# 划分数据为训练集合验证集
+X_train, X_test, y_train, y_test = model_selection.train_test_split(watermelonX, watermelonY, test_size = 0.4)
+print("y_test", y_test)
+
+
+# 训练数据，得到决策树
 clf = tree.DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 
-# Step 5，模型拟合测试集
+# 拟合验证集
 y_pred = clf.predict(X_test)
+print("y_pred", y_pred)
 
+#计算均方差
+print("MSE",metrics.mean_squared_error(y_test, y_pred))
 
 
 # 可视化决策树
 dot_data = tree.export_graphviz(clf, out_file=None)
 graph = graphviz.Source(dot_data)
-graph.render('waterMelon', '../docs/watermelon.pdf')
-
+graph.render('waterMelon', '../docs/watermelon')
