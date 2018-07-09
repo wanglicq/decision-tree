9c34d03f72733f8f73b290cc372917fe108417e0
wangli <wangli_cq@hotmail.com>
2018-07-09 19:21:44 +0800

    Remain original digital data.

--- src/watermelon.py
+++ src/watermelon.py
@@ -5,16 +5,16 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn import metrics
 import graphviz
+import numpy
 
-#
+#Read CSV file into DataFrame
 watermelonData = pd.read_csv('../docs/waterMelon.csv')
-
-data = pd.DataFrame(watermelonData)
-
 dataTransfor = LabelEncoder()
 
 #选择数据特性
-watermelonCharactor = data[['色泽', '根蒂', '敲声', '纹理', '脐部', '触感', '密度', '含糖率']].values
+watermelonCharactor = watermelonData[['色泽', '根蒂', '敲声', '纹理', '脐部', '触感']].values
+print(watermelonData)
+
 # 把字符串都转化为整型
 watermelonCharactor[:, 0] = dataTransfor.fit_transform(watermelonCharactor[:, 0])
 watermelonCharactor[:, 1] = dataTransfor.fit_transform(watermelonCharactor[:, 1])
@@ -22,22 +22,22 @@ watermelonCharactor[:, 2] = dataTransfor.fit_transform(watermelonCharactor[:, 2]
 watermelonCharactor[:, 3] = dataTransfor.fit_transform(watermelonCharactor[:, 3])
 watermelonCharactor[:, 4] = dataTransfor.fit_transform(watermelonCharactor[:, 4])
 watermelonCharactor[:, 5] = dataTransfor.fit_transform(watermelonCharactor[:, 5])
-watermelonCharactor[:, 6] = dataTransfor.fit_transform(watermelonCharactor[:, 6])
-watermelonCharactor[:, 7] = dataTransfor.fit_transform(watermelonCharactor[:, 7])
-#数据特性进行独热编码
-watermelonX = OneHotEncoder(categorical_features=[0], sparse=False).fit_transform(watermelonCharactor)
 
+#数据特性进行独热编码
+watermelonX = OneHotEncoder(sparse=False).fit_transform(watermelonCharactor)
+print(watermelonX.shape)
+print(watermelonData[['含糖率']].shape)
+watermelonX = numpy.concatenate((watermelonX, watermelonData[['密度']], watermelonData[['含糖率']]), axis=1)
+print(watermelonX)
+print("watermelonX\n", watermelonX.shape)
 
 #选择样本输出
-watermelonOutput = data[['好瓜']].values
+watermelonOutput = watermelonData[['好瓜']].values
 watermelonOutput[:, 0] = dataTransfor.fit_transform(watermelonOutput[:, 0])
-watermelonY = OneHotEncoder(categorical_features=[0], sparse=False).fit_transform(watermelonOutput)
-
+watermelonY = OneHotEncoder(sparse=False).fit_transform(watermelonOutput)
 
 # 划分数据为训练集合验证集
 X_train, X_test, y_train, y_test = model_selection.train_test_split(watermelonX, watermelonY, test_size = 0.4)
-print("y_test", y_test)
-
 
 # 训练数据，得到决策树
 clf = tree.DecisionTreeClassifier()
@@ -45,7 +45,6 @@ clf.fit(X_train, y_train)
 
 # 拟合验证集
 y_pred = clf.predict(X_test)
-print("y_pred", y_pred)
 
 #计算均方差
 print("MSE",metrics.mean_squared_error(y_test, y_pred))
